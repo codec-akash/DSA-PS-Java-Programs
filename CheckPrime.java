400413d4221bd33a4b3c335224150939def7ff5a
Piyush Nagpal <69682512+Piyushnagpal80@users.noreply.github.com>
2020-10-01 11:39:37 +0530

    Update CheckPrime.java

--- CheckPrime.java
+++ CheckPrime.java
@@ -1,20 +1,29 @@
 import java.util.*;
 public class CheckPrime{
     public  static void  main(String[] args) {
-        int x,count=0;
-        System.out.println("Enter the number");
-        Scanner input=new Scanner(System.in);
-        x=input.nextInt();
-        for(int i=1;i<=x;i++){
-            if(x%i==0){
-                count++;
-                    }
-                }            
-        if(count>2 || count==1){
-            System.out.println(x +" is not a prime number");
+       
+       Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+
+        boolean isprime = true;
+        for(int i=2; i*i<=n;i++){
+            if(n%i==0){
+                isprime = false;
+            break;
+            }
         }
-        else{
-            System.out.println(x +" is  a prime number");
+        if(n<2){
+                isprime = false;
+            }
+        if(isprime==false){
+        System.out.print(n+" is not a Prime Number");
+        }else{
+        System.out.print(n+" is a Prime Number");
         }
     }
 }
+        
+        
+       
