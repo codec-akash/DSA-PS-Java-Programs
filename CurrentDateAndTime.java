d2d365efe8f636ecbc5a2ca23eecc0402f8787b6
Charchit Mangal <63923359+charchithere@users.noreply.github.com>
2020-10-04 14:37:14 +0530

    Current Date and Time , Java Program

---
+++ CurrentDateAndTime.java
@@ -0,0 +1,7 @@
+public class CurrentDateAndTime{
+  public static void main(String args[]) {
+   LocalDateTime current = LocalDateTime.now();
+
+   System.out.println("Current Date and Time is: " + current) 
+  }
+}
